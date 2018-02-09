3bdbf2afe4e5b62e3863ee3c7096ae3724c306d1
Stephen Pham <pham.stephen@gmail.com>
2018-02-09 16:08:18 +1100

    Initialise delcom device

--- build_light.py
+++ build_light.py
@@ -1,13 +1,24 @@
+import sys
 import schedule
 import time
+from DelcomPython import DelcomUSBDevice
 
 PERIOD = 10
 
 def flash():
   print("hello world")
 
-schedule.every(PERIOD).seconds.do(flash)
+def initialise_light():
+  delcom = DelcomUSBDevice()
+  if delcom.find() == 0:
+    print("failed to find device")
+    sys.exit(0)
+  delcom.open()
+  print("device found:", delcom.DisplayInfo())
+  return delcom
 
+schedule.every(PERIOD).seconds.do(flash)
+initialise_light()
 while True:
   schedule.run_pending()
   time.sleep(1)
